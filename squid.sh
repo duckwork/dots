f31a504517e6e4849350e68454eaac4e9cd70d1a
Case Duckworth <cased123@gmail.com>
2016-01-24 18:20:40 -0600

    Remove cruft

--- squid.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Squid.sh. Scrape all patterns from squidfingers.com
-# TODO: Move this to ~/bin (and think about `git`ing that?)
-
-for i in {001..158}; do
-  wget http://www.squidfingers.com/_patterns/files/pattern_${i}.zip;
-  unzip patter_${i}.zip;
-  rm pattern_${i}.zip;
-done
